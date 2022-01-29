15d5a0c15889d9bf15569075ac912364316728db
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2022-01-29 12:26:31 -0300

    Initial setup

---
+++ PlatformService/Data/PrepareInMemoryDatabase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+using System;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    // Testing only, not for production.
+
+    public static class PrepareInMemoryDatabase
+    {
+        public static void PreparePopulation(IApplicationBuilder app)
+        {
+            using var servicesScope = app.ApplicationServices.CreateScope();
+
+            SeedData(servicesScope.ServiceProvider.GetService<AppDbContext>());
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding data...");
+
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "Dot net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "SQL Server Express",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Kubernetes",
+                        Publisher = "Cloud Native Computing Foundation",
+                        Cost = "Free"
+                    }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# net-microservices
-.NET Microservices – Full Course
+# .NET Microservices – Full Course
+
+Implementation of the course from Les Jackson: https://www.youtube.com/watch?v=DgVjEo3OGBI&t=11708s
+
+Current time: https://youtu.be/DgVjEo3OGBI?t=6079
