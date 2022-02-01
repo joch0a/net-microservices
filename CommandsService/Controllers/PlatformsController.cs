f8f4dbcca4b3c47aaff758b2b5bf31d98b446094
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2022-02-01 00:38:37 -0300

    Add command service

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboudConnection() 
+        {
+            Console.WriteLine("--> Inbound post # Command Service");
+
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
