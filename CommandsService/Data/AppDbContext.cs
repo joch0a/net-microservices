8d14bae0c9f9694a4eb74d6696ae269f825c48ae
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2022-02-03 16:00:08 -0300

    Data layer commandService

---
+++ CommandsService/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CommandsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandsService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+
+        public DbSet<Platform> Platforms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder
+                .Entity<Platform>()
+                .HasMany(platform => platform.Commands)
+                .WithOne(command => command.Platform!)
+                .HasForeignKey(command => command.PlatformId);
+
+            modelBuilder
+                .Entity<Command>()
+                .HasOne(command => command.Platform)
+                .WithMany(platform => platform.Commands)
+                .HasForeignKey(platform => platform.PlatformId);
+                
+        }
+    }
+}
